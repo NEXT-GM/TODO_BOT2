de0b8c59bbefaa6a4a6a0e6ef7e1aa9cca946d55
NEXT_GM <>
2021-05-03 15:46:03 +0000

    add bot files

---
+++ app.py
@@ -0,0 +1,6 @@
+import asyncio
+from aiogram import Bot, Dispatcher, executor
+
+from config import BOT_TOKEN
+
+loop = asyncio.get_ev
---
+++ config.py
@@ -0,0 +1,14 @@
+#https://api.telegram.org/bot1689730231:AAH-ZR_6YyH3NFu_s36KUIyiz1QgxVeEgSA/getUpdates
+
+admin_id = 589585815
+
+BOT_TOKEN = "1689730231:AAH-ZR_6YyH3NFu_s36KUIyiz1QgxVeEgSA"
+
+todo = {}
+
+HELP = """
+/help - выводить список команд
+/add - добавить задачу
+/show - показать задачи
+/done - убрать выполненную задачу
+"""
